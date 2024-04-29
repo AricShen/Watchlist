13b5aec092e9f0d76ba56e4cb8bd564802e0b9da
Aric <aric-jerrell.shen@outlook.com>
2024-04-29 21:14:33 +0800

    Add base template and error template

--- watchlist/app.py
+++ watchlist/app.py
@@ -16,11 +16,18 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的
 # 在扩展类实例化前加载配置
 db = SQLAlchemy(app)
 
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    return render_template('404.html',), 404  # 返回模板和状态码
 @app.route('/')
 def index():
-    user = User.query.first()  # 读取用户记录
     movies = Movie.query.all()  # 读取所有电影记录
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
 #test data
 # name = 'Grey Li'
 # movies = [
@@ -82,3 +89,6 @@ class Movie(db.Model):  # 表名将会是 movie
 
         db.session.commit()
         click.echo('Done.')
+
+
+
